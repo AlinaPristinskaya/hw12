988e9417554a7f8906225e917acf430bd5de024d
Alina Prystynska <pristinskayaalina9@gmail.com>
2024-04-13 01:13:08 +0100

    add map

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-// main.js
+
 const groceryItems = ['Apples', 'Milk', 'Bread', 'Eggs'];
-// main.js
+
 function displayItems() {
-    const ol = document.getElementById('grocery-list');
-    ol.innerHTML = ''; // Clear existing list items
+    const ul = document.querySelector('.grocery-list');
+    ul.innerHTML = ''; // Clear existing list items
 
-    groceryItems.forEach(item => {
-        const li = document.createElement('li');
-        li.textContent = item;
-        ol.appendChild(li);
+  const groceryItemsEl = groceryItems.map(item => {
+    
+        const liElement = document.createElement('li');
+        liElement.textContent = item;
+        return liElement
+        
     });
+    
+    ul.append(...groceryItemsEl)
 }
-
-// Call the function to display items when the page loads
 displayItems();
 
 function setDefaultChecked() {
-    const listItems = document.querySelectorAll('#grocery-list li');
+    const listItems = document.querySelectorAll('.grocery-list li');
     listItems[0].classList.add('bought');
     listItems[1].classList.add('bought');
 }
 
-// Call the function to set default checked items
 setDefaultChecked();
